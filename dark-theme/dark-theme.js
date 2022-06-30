155f93e205846bc3264a231cfb468732e3dbc245
JordonP123 <jordonaren@gmail.com>
2022-06-30 19:32:43 -0400

    Button Done

--- dark-theme/dark-theme.js
+++ dark-theme/dark-theme.js
@@ -1,1 +1,10 @@
 // you need to create DOM selectors to grab the correct HTML elements and update their styles to the opposite theme's css classes when you want to toggle the theme off and on
+const changeColor = document.querySelector('.container');
+const toggleOn = document.querySelector('.button');
+
+toggleOn.addEventListener('click', function(){
+    changeColor.classList.toggle('change');
+});
+
+
+
